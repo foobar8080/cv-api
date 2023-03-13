6963d4e656ad8b6b1c75c0d9777ad3067fadcb80
alex-31g <al.tsimbalyuk@gmail.com>
2023-03-13 22:48:28 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://capsuleverse-test.web.app"];
+  const origin = req.headers.origin as string;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  }
+  next();
+});
+
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",

f841d85d77906bbb28078f6f3dc2be89e5f8e2f2
alex-31g <al.tsimbalyuk@gmail.com>
2023-03-13 20:55:14 +0200

    Update

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var cors = require('cors');
+var cors = require("cors");
 var express_1 = __importDefault(require("express"));
 var get_route_1 = require("./get.route");
 var app = (0, express_1.default)();
-app.use(cors());
-app.route('/api/capsule-list/v1').get(get_route_1.getCapsuleList);
-app.route('/api/me/v1').get(get_route_1.getMe);
-app.route('/api/relations/v1').get(get_route_1.getRelations);
-app.route('/api/chats/v1/:chatId').get(get_route_1.getChats);
-app.route('/api/users/v1').get(get_route_1.getUsers);
-app.route('/api/live-chat/v1').get(get_route_1.getLiveChat);
+app.use(cors({ origin: "https://capsuleverse-test.web.app/" }));
+app.route("/api/capsule-list/v1").get(get_route_1.getCapsuleList);
+app.route("/api/me/v1").get(get_route_1.getMe);
+app.route("/api/relations/v1").get(get_route_1.getRelations);
+app.route("/api/chats/v1/:chatId").get(get_route_1.getChats);
+app.route("/api/users/v1").get(get_route_1.getUsers);
+app.route("/api/live-chat/v1").get(get_route_1.getLiveChat);
 var httpServer = app.listen(9000, function () {
-    console.log('HTTP REST API Server running at http://localhost:' +
+    console.log("HTTP REST API Server running at http://localhost:" +
         httpServer.address().port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const cors = require('cors');
-import express from 'express';
+const cors = require("cors");
+import express from "express";
 import {
   getCapsuleList,
   getMe,
@@ -7,21 +7,21 @@ import {
   getChats,
   getUsers,
   getLiveChat,
-} from './get.route';
+} from "./get.route";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://capsuleverse-test.web.app/" }));
 
-app.route('/api/capsule-list/v1').get(getCapsuleList);
-app.route('/api/me/v1').get(getMe);
-app.route('/api/relations/v1').get(getRelations);
-app.route('/api/chats/v1/:chatId').get(getChats);
-app.route('/api/users/v1').get(getUsers);
-app.route('/api/live-chat/v1').get(getLiveChat);
+app.route("/api/capsule-list/v1").get(getCapsuleList);
+app.route("/api/me/v1").get(getMe);
+app.route("/api/relations/v1").get(getRelations);
+app.route("/api/chats/v1/:chatId").get(getChats);
+app.route("/api/users/v1").get(getUsers);
+app.route("/api/live-chat/v1").get(getLiveChat);
 
 const httpServer: any = app.listen(9000, () => {
   console.log(
-    'HTTP REST API Server running at http://localhost:' +
+    "HTTP REST API Server running at http://localhost:" +
       httpServer.address().port
   );
 });
